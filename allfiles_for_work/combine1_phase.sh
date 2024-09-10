7c9add0feb3f1649c2f58162b5a98248235eeb24
kldsky <824417457@qq.com>
2024-09-10 12:58:43 +0200

    add allfiles for my work

---
+++ allfiles_for_work/combine1_phase.sh
@@ -0,0 +1,12 @@
+echo "date: $1"
+date=$1
+bash makeL_phase.sh ${date}_QPOphase 0001
+bash makeL_phase.sh ${date}_QPOphase 0102
+bash makeL_phase.sh ${date}_QPOphase 0203
+bash makeL_phase.sh ${date}_QPOphase 0304
+bash makeL_phase.sh ${date}_QPOphase 0405
+bash makeL_phase.sh ${date}_QPOphase 0506
+bash makeL_phase.sh ${date}_QPOphase 0607
+bash makeL_phase.sh ${date}_QPOphase 0708
+bash makeL_phase.sh ${date}_QPOphase 0809
+bash makeL_phase.sh ${date}_QPOphase 0910
---
+++ allfiles_for_work/cp0.sh
@@ -0,0 +1,1 @@
+cat obsid_20201208.list | awk '{print "cp /Volumes/WD_BLACK/Data/1A0535p262/HXMT/date_combine/20201208/"$1"/*E/1A*_bkg.pha 20201208_QPOphase/"$1"_phase"}'
